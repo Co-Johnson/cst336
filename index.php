f22dac7a5aec011b9b663c25134f678d5f6c9899
Corey Johnson <cojohnson@csumb.edu>
2018-10-22 23:24:58 +0000

    Contact link fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
     </head>
     <body>
-        <div class="header">
+        <div id="header">
             <?php
                 // A simple web site in Cloud9 that runs through Apache
                 // Press the 'Run' button on the top to start the web server,
@@ -15,16 +15,30 @@
                 
                 echo "<h1>Corey Johnson's CST336 Home Page</h1>";
             ?>
+            <br /><br />
+            <hr>
         </div>
-        <div class="content">
+        <div id="content">
             <div id="labs">
                 <h2>Labs</h2>
+                <hr>
                 <a href="labs/lab1/index.html">Lab 1</a>
             </div>
             <div id="homework">
                 <h2>Homework</h2>
+                <hr>
                 <a href="hw/hw1/index.html">Homework 1</a>
             </div>
+        </div>
+        <div id="footer">
+            <hr>
+            <br /><br />
+            <p>
+                CST 336 Internet Programming 2018 &copy; Johnson <br />
+                It is used for academic purposes only.
+                <br /><br />
+                <img src="img/csumb.png" alt="CSUMB logo">
+            </p>
         </div>
     </body>
 </html>
