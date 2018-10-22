b7c15b0d1f539782e960423a39b98a8e497fa85e
Corey Johnson <cojohnson@csumb.edu>
2018-10-22 18:36:05 +0000

    Validation update 1

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo "Corey Johnson's CST336 Home Page";
-
-
-
-?>
-
-<a href="labs/lab1/index.html">Lab 1</a>
-</body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CST 336: Corey Johnson - Team 9</title>
+        <meta charset="utf-8"/>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+    <body>
+        <div class="header">
+            <?php
+                // A simple web site in Cloud9 that runs through Apache
+                // Press the 'Run' button on the top to start the web server,
+                // then click the URL that is emitted to the Output tab of the console
+                
+                echo "<h1>Corey Johnson's CST336 Home Page</h1>";
+            ?>
+        </div>
+        <div class="content">
+            <div id="labs">
+                <h2>Labs</h2>
+                <a href="labs/lab1/index.html">Lab 1</a>
+            </div>
+            <div id="homework">
+                <h2>Homework</h2>
+                <a href="hw/hw1/index.html">Homework 1</a>
+            </div>
+        </div>
+    </body>
 </html>
